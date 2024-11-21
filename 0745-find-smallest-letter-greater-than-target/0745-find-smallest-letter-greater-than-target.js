7f2f7f731f57322c27461109449d53c591393539
ADITHYANKG <adithyankg9@gmail.com>
2024-11-21 20:45:40 +0530

    Time: 27 ms (6.58%), Space: 54.9 MB (5.24%) - LeetHub

---
+++ 0745-find-smallest-letter-greater-than-target/0745-find-smallest-letter-greater-than-target.js
@@ -0,0 +1,32 @@
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(l, t) {
+    res=l[0]
+    temp=res
+    console.log(temp)
+      for(i=0;i<l.length;i++){
+         if(l[i]<=t){
+            l.splice(i,1,"}")
+         }
+
+      }
+    console.log(l)
+    min=t.charCodeAt()
+    a="}"
+     s=a.charCodeAt()
+     
+     res=temp
+     console.log(res)
+     for(i=0;i<l.length;i++){
+          if((l[i].charCodeAt()-min)<s-min){
+            s=l[i].charCodeAt()-min
+            res=l[i]
+            
+          }
+
+     }
+    return res
+};
