ed9393f78961e1a80cca2463d97fbcd4b56fe436
ADITHYANKG <adithyankg9@gmail.com>
2024-12-16 20:06:40 +0530

    Time: 0 ms (100%), Space: 211.5 MB (27.49%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.rb
@@ -0,0 +1,13 @@
+# @param {Integer[]} nums1
+# @param {Integer} m
+# @param {Integer[]} nums2
+# @param {Integer} n
+# @return {Void} Do not return anything, modify nums1 in-place instead.
+def merge(nums1, m, nums2, n)
+ nums1.replace(nums1[0...m] + nums2[0...n])  # Combine and replace nums1
+  
+  # Sort nums1 in place
+  nums1.sort!
+
+    
+end
