5ad5a1dd2a93c5fc4c36a1a686dfa150cb97fd33
ADITHYANKG <adithyankg9@gmail.com>
2024-12-26 22:49:22 +0530

    Time: 0 ms (100%), Space: 212.2 MB (19.09%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.rb
@@ -0,0 +1,10 @@
+# @param {String} s
+# @return {String}
+def reverse_words(s)
+     words = s.split
+  # Reverse the array of words and join them with a single space
+  words.reverse.join(" ")
+
+    
+    
+end
