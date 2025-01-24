b8b9c89394ffdf1d4ef0002014afa969a786b1bd
ADITHYANKG <adithyankg9@gmail.com>
2025-01-24 22:35:37 +0530

    Time: 0 ms (100%), Space: 213.1 MB (66.67%) - LeetHub

---
+++ 2181-smallest-index-with-equal-value/2181-smallest-index-with-equal-value.rb
@@ -0,0 +1,12 @@
+# @param {Integer[]} nums
+# @return {Integer}
+def smallest_equal(nums)
+   for i in 0..nums.length
+      if i%10==nums[i]
+        return i
+        break
+      end
+    end
+    return -1      
+    
+end
