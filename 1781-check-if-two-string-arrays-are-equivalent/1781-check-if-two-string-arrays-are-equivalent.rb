903da5095e43e6b1b574539e9a02f79967e8384c
ADITHYANKG <adithyankg9@gmail.com>
2025-02-20 22:49:20 +0530

    Time: 0 ms (100%), Space: 211.6 MB (25%) - LeetHub

---
+++ 1781-check-if-two-string-arrays-are-equivalent/1781-check-if-two-string-arrays-are-equivalent.rb
@@ -0,0 +1,11 @@
+# @param {String[]} word1
+# @param {String[]} word2
+# @return {Boolean}
+def array_strings_are_equal(word1, word2)
+    if word1.join==word2.join
+    return true
+    
+    else
+    false
+    end
+end
