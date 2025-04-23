e95bc41f2a5f17a1b6291bd1d80bbafc877edc89
ADITHYANKG <adithyankg9@gmail.com>
2025-04-23 22:37:12 +0530

    Time: 0 ms (100%), Space: 12.2 MB (100%) - LeetHub

---
+++ 0782-jewels-and-stones/0782-jewels-and-stones.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numJewelsInStones(self, jewels, stones):
+        """
+        :type jewels: str
+        :type stones: str
+        :rtype: int
+        """
+        c=0
+        for i in range(len(stones)):
+            for j in range(len(jewels)):
+                if stones[i]==jewels[j]:
+                    c+=1
+                    break
+        return c            
+
+        
